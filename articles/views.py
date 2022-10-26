82ff5496c13da8b731ae901c28113b3316b104b1
poro625 <poro625@naver.com>
2022-10-26 09:40:38 +0900

    아침과제 제출

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from articles.models import Article
+from articles.serializers import ArticleSerializer
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET', 'POST'])    
+def index(request):
+    if request.method == 'GET':
+        article = Article.objects.all()
+        serializer = ArticleSerializer(article, many=True)
+        return Response(serializer.data) 
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else :
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
